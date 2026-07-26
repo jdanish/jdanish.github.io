44d7bd1fdeacf37c21421f319c5e8b01cf7cfa17
Danish, Joshua <jdanish@illinois.edu>
2026-07-25 19:53:43 -0500

    Added Jean's edges

--- gm/js/sidebar.js
+++ gm/js/sidebar.js
@@ -75,6 +75,48 @@ window.SIDEBAR_SECTIONS = [
             </div>
           </details>
 
+          <details class="subsection" data-persist-key="jean-grayson-edges">
+            <summary>Edges</summary>
+            <div class="subsection-body">
+                <ul class="note-list">
+                  <li>
+                    <a class="linkicon jump-link" href="#" data-tab="swade" data-page="38" data-highlight="FLEET-FOOTED">Fleet-Footed</a>:
+                    When sneaking behind enemy lines, it’s good to go fast. You have +2 Pace and a d8 Running die.
+                  </li>
+
+                  <li>
+                    <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="119" data-highlight="ARCANE BACKGROUND (PSY COMMANDO)">Arcane Background (Psy Commando)</a>:
+                    As a psychic warrior, your mind is a weapon. Enemy powers used against you suffer a –2 penalty to their rolls and deal 2 less damage.
+                  </li>
+
+                  <li>
+                    <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="120">Basic Psychic Loadout</a>:
+                    Even without your PsyFocus, you have the powers <em>arcane protection</em>, <em>boost/lower Trait</em> (boost only, self only), <em>smite</em> (self only), and <em>wall walker</em> (self only). You can also spend a Power Point to telepathically link up to 8 allies per the <em>mind link</em> power.
+                  </li>
+
+                  <li>
+                    <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="120">PsyFocus Loadout</a>:
+                    While wielding a PsyFocus, you have 5 additional Power Points and the powers <em>bolt</em>, <em>invisibility</em>, and <em>sound/silence</em>.
+                  </li>
+
+                  <li>
+                    <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="120">Advanced PsyFocus Loadout</a>:
+                    While wielding a PsyFocus, you have the powers <em>barrier</em>, <em>blast</em>, and <em>telekinesis</em>.
+                  </li>
+
+                  <li>
+                    <a class="linkicon jump-link" href="#" data-tab="starbreaker_star_marines" data-page="33">Living Weapon</a>:
+                    By channeling their psychic energies through their own flesh, psy commandos can enhance their natural capabilities. Once per encounter, as a limited free action, the psy-co may spend a Power Point to gain the benefits of one Combat Edge of his Rank or less, regardless of its other Requirements, until the end of the encounter. He can’t choose an Edge he already has, or that is currently active.
+                  </li>
+
+                  <li>
+                    <a class="linkicon jump-link" href="#" data-tab="starbreaker_star_marines" data-page="40">Heavy Hitters Squad Edge</a>:
+                    The squad is equipped with experimental weapons that occasionally punch well above their weight class. When a squad member makes an attack with a firearm, he can spend a Benny to declare that it’s a “heavy” attack. If the attack hits, it has +4 AP and deals Heavy Damage.
+                  </li>
+                </ul>
+            </div>
+          </details>
+
           <details class="subsection" data-persist-key="jean-grayson-armor">
             <summary>Armor</summary>
             <div class="subsection-body">
