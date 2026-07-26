f364fd042889bf09650e5d5eee46042c4ede7d34
Danish, Joshua <jdanish@illinois.edu>
2026-07-26 10:02:58 -0500

    Indented edges and added link to combat edges

--- gm/js/sidebar.js
+++ gm/js/sidebar.js
@@ -94,39 +94,47 @@ window.SIDEBAR_SECTIONS = [
             <div class="subsection-body">
                 <ul class="note-list">
                   <li>
-                    <a class="linkicon jump-link" href="#" data-tab="swade" data-page="38" data-highlight="FLEET-FOOTED">Fleet-Footed</a>:
-                    When sneaking behind enemy lines, it’s good to go fast. You have +2 Pace and a d8 Running die.
-                  </li>
+                    <a class="linkicon jump-link" href="#" data-tab="swade" data-page="38" data-highlight="FLEET-FOOTED">Fleet-Footed</a>
+                    <ul class="note-list">
+                    <li>
+                      When sneaking behind enemy lines, it’s good to go fast. You have +2 Pace and a d8 Running die.
+                  </li> </ul></li>
 
                   <li>
                     <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="119" data-highlight="ARCANE BACKGROUND (PSY COMMANDO)">Arcane Background (Psy Commando)</a>:
-                    As a psychic warrior, your mind is a weapon. Enemy powers used against you suffer a –2 penalty to their rolls and deal 2 less damage.
-                  </li>
+                    <ul class="note-list">
+                    <li>As a psychic warrior, your mind is a weapon. Enemy powers used against you suffer a –2 penalty to their rolls and deal 2 less damage.
+                  </li></ul></li>
 
                   <li>
                     <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="120">Basic Psychic Loadout</a>:
-                    Even without your PsyFocus, you have the powers <em>arcane protection</em>, <em>boost/lower Trait</em> (boost only, self only), <em>smite</em> (self only), and <em>wall walker</em> (self only). You can also spend a Power Point to telepathically link up to 8 allies per the <em>mind link</em> power.
-                  </li>
+                    <ul class="note-list">
+                    <li>Even without your PsyFocus, you have the powers <em>arcane protection</em>, <em>boost/lower Trait</em> (boost only, self only), <em>smite</em> (self only), and <em>wall walker</em> (self only). You can also spend a Power Point to telepathically link up to 8 allies per the <em>mind link</em> power.
+                  </li></ul></li>
 
                   <li>
                     <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="120">PsyFocus Loadout</a>:
-                    While wielding a PsyFocus, you have 5 additional Power Points and the powers <em>bolt</em>, <em>invisibility</em>, and <em>sound/silence</em>.
-                  </li>
+                    <ul class="note-list">
+                    <li>While wielding a PsyFocus, you have 5 additional Power Points and the powers <em>bolt</em>, <em>invisibility</em>, and <em>sound/silence</em>.
+                  </li></ul></li>
 
                   <li>
                     <a class="linkicon jump-link" href="#" data-tab="starbreaker_core" data-page="120">Advanced PsyFocus Loadout</a>:
-                    While wielding a PsyFocus, you have the powers <em>barrier</em>, <em>blast</em>, and <em>telekinesis</em>.
-                  </li>
+                    <ul class="note-list">
+                    <li>While wielding a PsyFocus, you have the powers <em>barrier</em>, <em>blast</em>, and <em>telekinesis</em>.
+                  </li></ul></li>
 
                   <li>
                     <a class="linkicon jump-link" href="#" data-tab="starbreaker_star_marines" data-page="33">Living Weapon</a>:
-                    By channeling their psychic energies through their own flesh, psy commandos can enhance their natural capabilities. Once per encounter, as a limited free action, the psy-co may spend a Power Point to gain the benefits of one Combat Edge of his Rank or less, regardless of its other Requirements, until the end of the encounter. He can’t choose an Edge he already has, or that is currently active.
-                  </li>
+                    <ul class="note-list">
+                    <li>By channeling their psychic energies through their own flesh, psy commandos can enhance their natural capabilities. Once per encounter, as a limited free action, the psy-co may spend a Power Point to gain the benefits of one Combat Edge (<a class="linkicon jump-link" href="#" data-tab="swade" data-page="59">SWADE</a>) of his Rank or less, regardless of its other Requirements, until the end of the encounter. He can’t choose an Edge he already has, or that is currently active.
+                  </li></ul></li>
 
                   <li>
                     <a class="linkicon jump-link" href="#" data-tab="starbreaker_star_marines" data-page="40">Heavy Hitters Squad Edge</a>:
-                    The squad is equipped with experimental weapons that occasionally punch well above their weight class. When a squad member makes an attack with a firearm, he can spend a Benny to declare that it’s a “heavy” attack. If the attack hits, it has +4 AP and deals Heavy Damage.
-                  </li>
+                    <ul class="note-list">
+                    <li>The squad is equipped with experimental weapons that occasionally punch well above their weight class. When a squad member makes an attack with a firearm, he can spend a Benny to declare that it’s a “heavy” attack. If the attack hits, it has +4 AP and deals Heavy Damage.
+                  </li></ul></li>
                 </ul>
             </div>
           </details>
@@ -301,9 +309,9 @@ window.SIDEBAR_SECTIONS = [
                     </li>
                   </ul>
                 </li>
-
               </ul>
 
+              
           </div>
           </details>
         `
